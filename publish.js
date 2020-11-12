f61979c691f75c87a4552ff3ab9801eea72392bb
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-12 09:57:38 -0300

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,3 +1,9 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish('build', function (err) { });
+ghpages.publish('build', function (err) { 
+  if (err) {
+    throw err;
+  }
+  
+  console.log('Success');
+});
