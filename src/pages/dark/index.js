ba228e13f3b21d5e2778b0ac75cb8841c3fcbd20
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-13 08:56:38 -0300

    Update index.js

--- src/pages/dark/index.js
+++ src/pages/dark/index.js
@@ -3,45 +3,45 @@ import IndexTemplate from "../index-template"
 import { generateRandomColor, getValidRgbValue } from "../../scripts/utils";
 
 function generatePalette() {
-    const base = generateRandomColor({
-        r: [0, 160],
-        g: [0, 160],
-        b: [0, 160],
+  const base = generateRandomColor({
+    r: [0, 160],
+    g: [0, 160],
+    b: [0, 160],
     });
-    const baseDark = generateRandomColor({
-        r: [0, base.color.r / 2],
-        g: [0, base.color.g / 2],
-        b: [0, base.color.b / 2],
+  const baseDark = generateRandomColor({
+    r: [0, base.color.r / 2],
+    g: [0, base.color.g / 2],
+    b: [0, base.color.b / 2],
     });
-    const baseSemiDark = generateRandomColor({
-        r: [base.color.r / 2, base.color.r],
-        g: [base.color.g / 2, base.color.g],
-        b: [base.color.b / 2, base.color.b],
+  const baseSemiDark = generateRandomColor({
+    r: [base.color.r / 2, base.color.r],
+    g: [base.color.g / 2, base.color.g],
+    b: [base.color.b / 2, base.color.b],
     });
-    const baseSemiLight = generateRandomColor({
-        r: [base.color.r, getValidRgbValue(base.color.r + 30)],
-        g: [base.color.g, getValidRgbValue(base.color.g + 30)],
-        b: [base.color.b, getValidRgbValue(base.color.b + 30)],
+  const baseSemiLight = generateRandomColor({
+    r: [base.color.r, getValidRgbValue(base.color.r + 30)],
+    g: [base.color.g, getValidRgbValue(base.color.g + 30)],
+    b: [base.color.b, getValidRgbValue(base.color.b + 30)],
     });
-    const baseLight = generateRandomColor({
-        r: [getValidRgbValue(base.color.r + 30), 255],
-        g: [getValidRgbValue(base.color.g + 30), 255],
-        b: [getValidRgbValue(base.color.b + 30), 255],
-    });
+  const baseLight = generateRandomColor({
+    r: [getValidRgbValue(base.color.r + 30), 255],
+    g: [getValidRgbValue(base.color.g + 30), 255],
+    b: [getValidRgbValue(base.color.b + 30), 255],
+  });
 
-    return {
-        baseDark,
-        baseSemiDark,
-        base,
-        baseSemiLight,
-        baseLight
-    }
+  return {
+    baseDark,
+    baseSemiDark,
+    base,
+    baseSemiLight,
+    baseLight
+  }
 }
 
 const DarkPage = {
-    render: function () {
-        IndexTemplate.render(generatePalette);
-    }
+  render: function () {
+    IndexTemplate.render(generatePalette);
+  }
 }
 
 export default DarkPage;
