262f83ba75d7c5182818845545aed46589d086d6
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-15 13:24:00 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,103 +1,108 @@
 import { getLiquidRoute } from "../scripts/utils";
 
 const Router = function () {
-    const instance = {
-        routes: [],
+  const instance = {
+    routes: [],
 
-        pathname: null,
+    pathname: null,
 
-        page404: null,
+    page404: null,
 
-        match: function () {
-            const { pathname } = this;
+    match: function () {
+      const { pathname } = this;
 
-            const useIndex = pathname === "/";
+      const useIndex = pathname === "/";
 
-            if (useIndex) return this.routes.forEach(route => route.pathname === "/" && route.callback());
+      if (useIndex)
+        return this.routes.forEach(
+          (route) => route.pathname === "/" && route.callback()
+        );
 
-            const params = pathname.divide("/");
+      const params = pathname.divide("/");
 
-            let req = {
-                params: null,
-            }
-            let useReq = false;
+      let req = {
+        params: null,
+      };
+      let useReq = false;
 
-            let is404 = 0;
+      let is404 = 0;
 
-            this.routes.filter(route => route.pathname !== "/").forEach(route => {
-                const routeParams = route.pathname.divide("/");
-                if (routeParams.length !== params.length) return;
-                let isTheRoute = true;
-                routeParams.forEach((item, i) => {
-                    const isParam = item.charAt(0) === ":";
-                    const paramName = item.slice((isParam ? 1 : 0), item.length);
-                    if (isParam) {
-                        req.params = Object.assign({}, req, {
-                            ...req.params,
-                            params: {
-                                [paramName]: params[i],
-                            }
-                        });
-                        useReq = true;
-                        return;
-                    }
-                    if (paramName !== params[i]) isTheRoute = false;
-                });
-                if (isTheRoute) is404++;
-
-                return isTheRoute && route.callback(useReq ? req : undefined);
-            });
-            if (is404 === 0) this.page404.callback();
-        },
-        get: function (pathname, callback) {
-            if (!pathname || !callback) {
-                throw new Error("No Route or Callback provided :(");
-            }
-            if (typeof pathname !== "string") {
-                throw new Error("The Route must be a string :(");
-            }
-            if (this.routes.some(route => route.pathname === pathname)) {
-                throw new Error("Already have a route with this path :(");
-            }
-
-            if (pathname === "*") {
-                this.page404 = {
-                    pathname,
-                    callback
-                }
-                return;
-            }
-
-            this.routes.push({
-                pathname,
-                callback
-            });
-        },
-        setPathname: function (newPathname) {
-            this.pathname = getLiquidRoute(newPathname);
-        },
-        redirectTo: function (pathname) {
-            this.setPathname(pathname);
-            history.pushState({}, pathname, pathname);
-            this.match();
-        },
-        init: function () {
-            if (!window) {
+      this.routes
+        .filter((route) => route.pathname !== "/")
+        .forEach((route) => {
+          const routeParams = route.pathname.divide("/");
+          if (routeParams.length !== params.length) return;
+          let isTheRoute = true;
+          routeParams.forEach((item, i) => {
+            const isParam = item.charAt(0) === ":";
+            const paramName = item.slice(isParam ? 1 : 0, item.length);
+            if (isParam) {
+              req.params = Object.assign({}, req, {
+                ...req.params,
+                params: {
+                  [paramName]: params[i],
+                },
+              });
+              useReq = true;
               return;
             }
+            if (paramName !== params[i]) isTheRoute = false;
+          });
+          if (isTheRoute) is404++;
 
-            this.setPathname(window.location.pathname);
+          return isTheRoute && route.callback(useReq ? req : undefined);
+        });
+      if (is404 === 0) this.page404.callback();
+    },
+    get: function (pathname, callback) {
+      if (!pathname || !callback) {
+        throw new Error("No Route or Callback provided :(");
+      }
+      if (typeof pathname !== "string") {
+        throw new Error("The Route must be a string :(");
+      }
+      if (this.routes.some((route) => route.pathname === pathname)) {
+        throw new Error("Already have a route with this path :(");
+      }
 
-            window.onpopstate = function () {
-                this.setPathname(window.location.pathname);
-                this.match.call(this);
-            }.bind(this);
+      if (pathname === "*") {
+        this.page404 = {
+          pathname,
+          callback,
+        };
+        return;
+      }
 
-            this.match();
-        }
-    }
+      this.routes.push({
+        pathname,
+        callback,
+      });
+    },
+    setPathname: function (newPathname) {
+      this.pathname = getLiquidRoute(newPathname);
+    },
+    redirectTo: function (pathname) {
+      this.setPathname(pathname);
+      history.pushState({}, pathname, pathname);
+      this.match();
+    },
+    init: function () {
+      if (!window) {
+        return;
+      }
 
-    return instance;
-}
+      this.setPathname(window.location.pathname);
+
+      window.onpopstate = function () {
+        this.setPathname(window.location.pathname);
+        this.match.call(this);
+      }.bind(this);
+
+      this.match();
+    },
+  };
+
+  return instance;
+};
 
 export default Router;
