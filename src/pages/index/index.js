c90f76372cb27436df1f7315dd41998699250cf2
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-18 11:48:39 -0300

    Update index.js

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,48 +1,47 @@
-import IndexTemplate from "../index-template"
-
+import IndexTemplate from "../index-template";
 
 import { generateRandomColor, getValidRgbValue } from "../../scripts/utils";
 
 function generatePalette() {
-    const base = generateRandomColor();
-  
-    const baseDark = generateRandomColor({
-        r: [0, base.color.r / 2],
-        g: [0, base.color.g / 2],
-        b: [0, base.color.b / 2],
-    });
-  
-    const baseSemiDark = generateRandomColor({
-        r: [base.color.r / 2, base.color.r],
-        g: [base.color.g / 2, base.color.g],
-        b: [base.color.b / 2, base.color.b],
-    });
-  
-    const baseSemiLight = generateRandomColor({
-        r: [base.color.r, getValidRgbValue(base.color.r + 30)],
-        g: [base.color.g, getValidRgbValue(base.color.g + 30)],
-        b: [base.color.b, getValidRgbValue(base.color.b + 30)],
-    });
-  
-    const baseLight = generateRandomColor({
-        r: [getValidRgbValue(base.color.r + 30), 255],
-        g: [getValidRgbValue(base.color.g + 30), 255],
-        b: [getValidRgbValue(base.color.b + 30), 255],
-    });
+  const base = generateRandomColor();
 
-    return {
-        baseDark,
-        baseSemiDark,
-        base,
-        baseSemiLight,
-        baseLight
-    }
+  const baseDark = generateRandomColor({
+    r: [0, base.color.r / 2],
+    g: [0, base.color.g / 2],
+    b: [0, base.color.b / 2],
+  });
+
+  const baseSemiDark = generateRandomColor({
+    r: [base.color.r / 2, base.color.r],
+    g: [base.color.g / 2, base.color.g],
+    b: [base.color.b / 2, base.color.b],
+  });
+
+  const baseSemiLight = generateRandomColor({
+    r: [base.color.r, getValidRgbValue(base.color.r + 30)],
+    g: [base.color.g, getValidRgbValue(base.color.g + 30)],
+    b: [base.color.b, getValidRgbValue(base.color.b + 30)],
+  });
+
+  const baseLight = generateRandomColor({
+    r: [getValidRgbValue(base.color.r + 30), 255],
+    g: [getValidRgbValue(base.color.g + 30), 255],
+    b: [getValidRgbValue(base.color.b + 30), 255],
+  });
+
+  return {
+    baseDark,
+    baseSemiDark,
+    base,
+    baseSemiLight,
+    baseLight,
+  };
 }
 
 const IndexPage = {
-    render: function () {
-        IndexTemplate.render(generatePalette);
-    }
-}
+  render: function () {
+    IndexTemplate.render(generatePalette);
+  },
+};
 
 export default IndexPage;
