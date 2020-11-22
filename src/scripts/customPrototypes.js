d9b6cf9535ecd3fff6ad82aab5dcdaa518315171
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-22 09:57:17 -0300

    Update customPrototypes.js

--- src/scripts/customPrototypes.js
+++ src/scripts/customPrototypes.js
@@ -1,10 +1,10 @@
 String.prototype.divide = function (stringToDivide) {
-    return this.split(stringToDivide).filter(item => item !== "");
+  return this.split(stringToDivide).filter(item => item !== "");
 }
 
 String.prototype.stringToHTML = function () {
-    const str = this;
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(str, 'text/html');
-    return doc.body.firstChild;
+  const str = this;
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, 'text/html');
+  return doc.body.firstChild;
 }
