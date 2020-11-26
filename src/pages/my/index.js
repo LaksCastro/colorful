a4b0ce3c15ccbe8f627a8c788a759b6128bb5101
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-26 13:01:45 -0400

    Update index.js

--- src/pages/my/index.js
+++ src/pages/my/index.js
@@ -1,53 +1,63 @@
-import IndexTemplate from "../index-template"
+import IndexTemplate from "../index-template";
 
 import "../../assets/images/void.svg";
 
 import "./styles.scss";
 
-import { useLocalStorage } from "../../scripts/useLocalStorage"
+import { useLocalStorage } from "../../scripts/useLocalStorage";
 
 let palettes = null;
 
 const noPalettesString = `
     <div id="no-palette-wrapper">
-        <img src="${process.env.PATH_BASE === "/" ?
-        "/assets/void.svg" : `${process.env.PATH_BASE}/assets/void.svg`}" />
+        <img src="${
+          process.env.PATH_BASE === "/"
+            ? "/assets/void.svg"
+            : `${process.env.PATH_BASE}/assets/void.svg`
+        }" />
         <h1>The beautiful palettes you save will appear here</h1>
         <h1>As belas paletas que você salvar aparecerão aqui</h1>
     </div>
-`
+`;
 const noPalettesNode = noPalettesString.stringToHTML();
 
 function renderPalette() {
-    if (this.currentRenderIndex === palettes.length - 1) return this.loadMore = () => { };
+  if (this.currentRenderIndex === palettes.length - 1)
+    return (this.loadMore = () => {});
 
-    let paletteArray = palettes[this.currentRenderIndex];
+  let paletteArray = palettes[this.currentRenderIndex];
 
-    this.index.addPalette(
-        {
-            hex: paletteArray.colors,
-        },
-        paletteArray.id
-    );
+  this.index.addPalette(
+    {
+      hex: paletteArray.colors,
+    },
+    paletteArray.id
+  );
 
-    this.currentRenderIndex++;
+  this.currentRenderIndex++;
 }
 
 const MyPalettesPage = {
-    render: function () {
-        palettes = useLocalStorage().getPalettes();
-        palettes.reverse();
+  render: function () {
+    palettes = useLocalStorage().getPalettes();
+    palettes.reverse();
 
-        if (!palettes.length) {
-            IndexTemplate.render(undefined, undefined, undefined, noPalettesNode, () => { });
-        } else {
-            if (palettes.length <= 20) {
-                IndexTemplate.render(undefined, useLocalStorage().getPalettes());
-            } else {
-                IndexTemplate.render(undefined, undefined, renderPalette);
-            }
-        }
-    },
-}
+    if (!palettes.length) {
+      IndexTemplate.render(
+        undefined,
+        undefined,
+        undefined,
+        noPalettesNode,
+        () => {}
+      );
+    } else {
+      if (palettes.length <= 20) {
+        IndexTemplate.render(undefined, useLocalStorage().getPalettes());
+      } else {
+        IndexTemplate.render(undefined, undefined, renderPalette);
+      }
+    }
+  },
+};
 
 export default MyPalettesPage;
