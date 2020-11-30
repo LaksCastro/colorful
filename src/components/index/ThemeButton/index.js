516f4b885540496dda4726b813513d9d1c05f077
LaksCastro <lakhsmycastro336@gmail.com>
2020-11-30 08:19:56 -0400

    Update index.js

--- src/components/index/ThemeButton/index.js
+++ src/components/index/ThemeButton/index.js
@@ -1,24 +1,24 @@
 import "./styles.scss";
 
 function ThemeButton() {
-    const buttonString = `
+  const buttonString = `
         <button id="theme-button">
             <i class="fas fa-paint-roller"></i>
         </button>
-    `
-    
-    const buttonNode = buttonString.stringToHTML();
-    buttonNode.onclick = () => {
+    `;
+
+  const buttonNode = buttonString.stringToHTML();
+  buttonNode.onclick = () => {
+    window.__toggleTransitionSpan();
+    setTimeout(() => {
+      window.__setPreferredTheme(window.__theme === "dark" ? "light" : "dark");
+      setTimeout(() => {
         window.__toggleTransitionSpan();
-        setTimeout(() => {
-            window.__setPreferredTheme(window.__theme === "dark" ? "light" : "dark");
-            setTimeout(() => {
-                window.__toggleTransitionSpan();
-            }, 300);
-        }, 300);
-    };
-  
-    return buttonNode;
+      }, 300);
+    }, 300);
+  };
+
+  return buttonNode;
 }
 
 export default ThemeButton;
