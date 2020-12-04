79d488faaeab15ed1c2c010ba628b8755a4fc423
LaksCastro <lakhsmycastro336@gmail.com>
2020-12-04 09:23:06 -0400

    Update index.js

--- src/pages/page404/index.js
+++ src/pages/page404/index.js
@@ -4,26 +4,29 @@ import "../../assets/images/404.svg";
 
 import { removeAllChildsOf } from "../../scripts/utils";
 
-import { redirect } from "../../scripts/redirectTo"
+import { redirect } from "../../scripts/redirectTo";
 
 const IndexPage = {
-    render: function () {
-
-        const page404 = `
+  render: function () {
+    const page404 = `
             <div id="page-404">
                 <h1>40</h1>
                 <p>Nothing here, sorry</p>
                 <button id="go-back-button">Go Back</button>
-                <img src="${process.env.PATH_BASE === "/" ? "/assets/404.svg" : `${process.env.PATH_BASE}/assets/404.svg`}" />
+                <img src="${
+                  process.env.PATH_BASE === "/"
+                    ? "/assets/404.svg"
+                    : `${process.env.PATH_BASE}/assets/404.svg`
+                }" />
             </div>
-        `
-        const rootElement = document.getElementById("root")
+        `;
+    const rootElement = document.getElementById("root");
 
-        removeAllChildsOf(rootElement);
-        rootElement.appendChild(page404.stringToHTML());
+    removeAllChildsOf(rootElement);
+    rootElement.appendChild(page404.stringToHTML());
 
-        document.getElementById("go-back-button").onclick = () => redirect("/");
-    }
-}
+    document.getElementById("go-back-button").onclick = () => redirect("/");
+  },
+};
 
 export default IndexPage;
