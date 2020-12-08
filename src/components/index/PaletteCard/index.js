32aadcae09b2bb3d16dabe3a076d1f549f5b753f
LaksCastro <lakhsmycastro336@gmail.com>
2020-12-08 09:22:56 -0400

    Update index.js

--- src/components/index/PaletteCard/index.js
+++ src/components/index/PaletteCard/index.js
@@ -1,28 +1,28 @@
 import "./styles.scss";
 import Hammer from "hammerjs";
-import SimpleToast from "../SimpleToast"
-import { copyToClipboard } from "../../../scripts/utils"
+import SimpleToast from "../SimpleToast";
+import { copyToClipboard } from "../../../scripts/utils";
 
 function PaletteCard(color) {
-    const colorString = `<p>${color}</p>`
-    const colorNode = colorString.stringToHTML();
+  const colorString = `<p>${color}</p>`;
+  const colorNode = colorString.stringToHTML();
 
-    const paletteString = `
+  const paletteString = `
         <div style="background-color: ${color}" class="p-item"></div>
-    `
-    
-    const paletteNode = paletteString.stringToHTML();
+    `;
 
-    const colorHammer = new Hammer(colorNode);
-  
-    colorHammer.on("tap", () => {
-        copyToClipboard(color);
-        SimpleToast("Copied to clipboard!");
-    });
+  const paletteNode = paletteString.stringToHTML();
 
-    paletteNode.appendChild(colorNode);
+  const colorHammer = new Hammer(colorNode);
 
-    return paletteNode;
+  colorHammer.on("tap", () => {
+    copyToClipboard(color);
+    SimpleToast("Copied to clipboard!");
+  });
+
+  paletteNode.appendChild(colorNode);
+
+  return paletteNode;
 }
 
 export default PaletteCard;
